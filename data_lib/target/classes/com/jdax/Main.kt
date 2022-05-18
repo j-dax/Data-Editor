0b622ad85580b64c6ef672121b36973a36d93fa6
j-dax <67487509+j-dax@users.noreply.github.com>
2022-05-18 05:32:01 -0700

    removed compile directory from data_lib

--- data_lib/target/classes/com/jdax/Main.kt
+++
@@ -1,5 +0,0 @@
-package com.jdax
-
-public fun main(args: Array<String>) {
-    println("OK")
-}
--- data_lib/target/classes/com/jdax/data/window/DataWindow.kt
+++
@@ -1,5 +0,0 @@
-package com.jdax.data.window
-
-data class DataWindow constructor(val content: List<List<String>>) {
-    constructor(arrayContent: Array<Array<String>>) : this(arrayContent.map { it.asList() })
-}
